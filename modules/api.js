3f7e0f2febd56eb2003c1f0b3014b8dcb44b0887
EvGus52 <evgenijguskov247@gmail.com>
2025-06-14 20:25:36 +0300

    fh

---
+++ modules/api.js
@@ -0,0 +1,31 @@
+const host = 'https://wedev-api.sky.pro/api/v1/EvGus52'
+export const fetchComments = () => {
+    return fetch(host + '/comments')
+        .then((response) => {
+            return response.json()
+        })
+        .then((responseData) => {
+            const appComments = responseData.comments.map((comment) => {
+                return {
+                    name: comment.author.name,
+                    date: new Date(comment.date),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false,
+                }
+            })
+            return appComments
+        })
+}
+
+export const addComment = (text, name) => {
+    return fetch(host + './comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            text: text,
+            name: name,
+        }),
+    }).then(() => {
+        return fetchComments()
+    })
+}
